eb87373d3a0bf616a83cc6be3b1e2f78ebd0eb02
Cai The Si <caithesi21041997@gmail.com>
2024-10-30 19:18:10 +0700

    change query to get guestbook

--- web-dev/guestbook/resources/sql/queries.sql
+++ web-dev/guestbook/resources/sql/queries.sql
@@ -1,3 +1,14 @@
+-- :name save-message! :! :n
+-- :doc create new message using name nad message keys
+INSERT INTO guestbook
+(name, message)
+VALUES (:name, :message)
+
+-- :name get-messages :? :*
+-- :doc select all avaialble messages
+SELECT * from guestbook
+
+
 -- :name create-user! :! :n
 -- :doc creates a new user record
 INSERT INTO users
